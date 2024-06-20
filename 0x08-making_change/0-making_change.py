82f1066ac2836d441fed7ad8ac4a55f3986bc5e9
wishon1 <wisdomhonest1@gmail.com>
2024-06-20 16:19:38 -0400

    add the code

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,14 +4,16 @@ Interview Question on: fewest number of coins needed to
 meet a given amount total using greedy method
 """
 
+
 def makeChange(coins, total):
-    """ Return the fewest number of coins needed to meet total using a greedy method """
+    """ Return the fewest number of coins needed to meet
+    total using a greedy method """
     if total <= 0:
         return 0
 
     # Sort the coins in descending order
     coins.sort(reverse=True)
-    
+
     count = 0
     for coin in coins:
         if total == 0:
